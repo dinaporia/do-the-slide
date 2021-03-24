8bb56f223f6eac1640d505c454083a71265e777f
Dina <dmuhic@gmail.com>
2021-03-24 21:43:58 +0100

    img

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
 				<p className='left'>Do the Slide is a puzzle game built in the style of slide-15 puzzles. The puzzle will randomly select an image, divide it up into tiles, and scramble those tiles on the board.  </p>
 				
 				<p className='right'>Your job, as the player, is to rearrange the tiles back into the original image. Click on a tile to move it - but you can only move tiles that are adjacent to the empty slot. See if you can do it in fewer than 500 moves!</p>
-				<img className='center' src='gh-ages/images/demo.gif' alt='animated screencap of game'/>
+				<img className='center' src='./images/demo.gif' alt='animated screencap of game'/>
 			
 				<p className='left'>This app was built by <a href='https://dinamuhic.com' target='_blank' rel='noreferrer'> Dina Muhic</a>, <br />using React and Sass, <br /> as part of a <a href='https://mintbean.io/' target='_blank' rel='noreferrer'>MintBean</a> learnathon project.</p>
 				<p className='right'>You can view the source code <a href='https://github.com/dinaporia/do-the-slide' target='_blank' rel='noreferrer'>here</a>.</p>
